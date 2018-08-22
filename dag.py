2e4c177f7a7d38a3249231c6ac5119978ec9a708
Kevin Mann <KevinMann13@gmail.com>
2018-08-22 10:49:08 -0500

    Update dag.py

--- dag.py
+++ dag.py
@@ -15,3 +15,8 @@ dag = DAG(dag_id='example_bash_operator', default_args=args)
 
 run_this = BashOperator(
     task_id='echo_hello_world', bash_command='echo "hello world 123"', dag=dag)
+
+run_that = BashOperator(
+    task_id='sleepy', bash_command='sleep 2m', dag=dag)
+
+run_this.set_upstream(run_that)
